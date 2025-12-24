d60c8efddc63b47dac8baa28b2e13138ef9e5a7e
임창종 <aaaa3278@naver.com>
2025-12-24 16:20:19 +0900

    12241620

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,15 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './entities/user.entity';
+import { Roles } from 'src/roles/entities/role.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users)
     private readonly usersRepository: Repository<Users>,
+    @InjectRepository(Roles)
+    private readonly rolesRepository: Repository<Roles>,
   ) {}
 
   // 이메일로 유저 찾기 (로그인에서 사용)
@@ -38,13 +41,28 @@ export class UsersService {
     });
   }
 
-  // 유저 생성
+  // 유저 생성 (회원가입 시 USER 역할 자동 부여)
   async create(userData: {
     email: string;
     password: string;
     nickname?: string;
   }): Promise<Users> {
-    const user = this.usersRepository.create(userData);
+    // USER 역할 조회
+    const userRole = await this.rolesRepository.findOne({
+      where: { name: 'USER' },
+    });
+
+    if (!userRole) {
+      throw new Error('USER 역할이 존재하지 않습니다.');
+    }
+
+    // 유저 생성 + 역할 지정
+    const user = this.usersRepository.create({
+      ...userData,
+      roles: [userRole],
+    });
+
+    // 저장 → user_roles 자동 생성
     return this.usersRepository.save(user);
   }
 }
